bd29f69d15dbf98aeab5bbdf906a32fe09d07af2
Michal Miszczyszyn <michal@mmiszy.pl>
2019-11-12 17:39:14 +0100

    Updates

---
+++ precache-manifest.60c39e8cc4e4518031f0c1c3b32b8465.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "842ccbfb3a9502b61333855c85aec0ec",
+    "url": "/investmentday-mobx/index.html"
+  },
+  {
+    "revision": "24e715e740835ab7d13c",
+    "url": "/investmentday-mobx/static/css/main.3ec3f507.chunk.css"
+  },
+  {
+    "revision": "29c4ed64013e96c89ed4",
+    "url": "/investmentday-mobx/static/js/2.5f8d21ac.chunk.js"
+  },
+  {
+    "revision": "24e715e740835ab7d13c",
+    "url": "/investmentday-mobx/static/js/main.00fcb455.chunk.js"
+  },
+  {
+    "revision": "3cde5d3801fe0229a951",
+    "url": "/investmentday-mobx/static/js/runtime-main.a894623f.js"
+  }
+]);
